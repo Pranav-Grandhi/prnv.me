1649c154be681664e82fe2481155d8a3048c154b
Pranav Grandhi <pranavgrandhi@Pranavs-MacBook-Pro.local>
2023-10-20 16:40:26 -0500

    filter by change type

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,14 +1,18 @@
+import * as React from "react";
 import { json } from "@remix-run/node";
-import { useLoaderData } from "@remix-run/react";
+import { useLoaderData, useSearchParams } from "@remix-run/react";
 import fs from "fs";
 import path from "path";
-import { array, object, string, validate } from "superstruct";
+import { array, object, string, enums, validate } from "superstruct";
+
+const changeTypes = ["Me", "Projects", "Writing"];
 
 const Changes = array(
   object({
     title: string(),
     description: string(),
     occurrence: string(),
+    type: enums(changeTypes),
   })
 );
 
@@ -43,24 +47,67 @@ export const loader = () => {
 
 export default function Index() {
   const { changes } = useLoaderData<typeof loader>();
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  const changeTypesParam = searchParams.get("changeTypes");
+  const selectedChangeTypes = changeTypesParam
+    ? changeTypesParam.split("+").map((item) => item.trim())
+    : changeTypes;
+
+  const filteredChanges = React.useMemo(() => {
+    for (let i = 0; i < selectedChangeTypes.length; i++) {
+      if (!changeTypes.includes(selectedChangeTypes[i]))
+        throw new Error(`Invalid change type: '${selectedChangeTypes[i]}'`);
+    }
+
+    return changes.filter((change) =>
+      selectedChangeTypes.includes(change.type)
+    );
+  }, [selectedChangeTypes, changes]);
+
+  const toggleChangeType = (changeType: string) => {
+    let updatedChangeTypes = selectedChangeTypes;
+    if (selectedChangeTypes.includes(changeType)) {
+      updatedChangeTypes = updatedChangeTypes.filter(
+        (type) => type !== changeType
+      );
+    } else {
+      updatedChangeTypes.push(changeType);
+    }
+
+    const params = new URLSearchParams();
+    if (updatedChangeTypes.length > 0) {
+      params.set("changeTypes", updatedChangeTypes.join("+"));
+    }
+    setSearchParams(params);
+  };
 
   return (
     <div>
-      {changes.map(({ title, description, occurrence }) => {
-        const formattedDate = new Date(occurrence).toLocaleString("en-us", {
-          month: "long",
-          day: "numeric",
-          year: "numeric",
-        });
+      <div>
+        {changeTypes.map((changeType) => (
+          <button key={changeType} onClick={() => toggleChangeType(changeType)}>
+            {changeType}
+          </button>
+        ))}
+      </div>
+      <div>
+        {filteredChanges.map(({ title, description, occurrence }) => {
+          const formattedDate = new Date(occurrence).toLocaleString("en-us", {
+            month: "long",
+            day: "numeric",
+            year: "numeric",
+          });
 
-        return (
-          <article key={title}>
-            <time dateTime={occurrence}>{formattedDate}</time>
-            <h2>{title}</h2>
-            <p>{description}</p>
-          </article>
-        );
-      })}
+          return (
+            <article key={title}>
+              <time dateTime={occurrence}>{formattedDate}</time>
+              <h2>{title}</h2>
+              <p>{description}</p>
+            </article>
+          );
+        })}
+      </div>
     </div>
   );
 }
